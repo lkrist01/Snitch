0760d10236d8c16920fb6bde1e1693af92f13d74
lkrist01 <kristilici@gmail.com>
2020-11-04 13:06:10 +0000

    Deleting

--- covid/static/fetch_data.js
+++
@@ -1,9 +0,0 @@
-var all_data;
-$.ajax({
-  url: 'get_all_data'+window.location.search,//add date as param
-  method: 'get',
-  async: false, // notice this line
-  success: (data) => {
-    all_data = data;
-  }
-});
